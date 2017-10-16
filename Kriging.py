ce8bce98bbe6d74785cab4bd199ff52db27f881f
SGD <1770480953@qq.com>
2017-10-16 18:00:27 +0800

    debug 1

--- Kriging.py
+++ Kriging.py
@@ -5,56 +5,113 @@
 
 
 import numpy as np
+import KrigingPackage.DE as DE
 import matplotlib.pyplot as plt
-import GlobalOptimizeMethod.DOE as doe
-from matplotlib import cm
+import KrigingPackage.DOE as DOE
+import mayavi.mlab as mlab
+import os
+os.environ['QT_API']='pyside'
+
 
 class Kriging(object):
     def __init__(self,dataPoints,value,min=None,max=None):
         """points是插值点，n行d列，d是点的维度，n是点的数目
-        min和max是拟合空间的范围，用以对数据进行归一化。如果min和max都等于0，说明点数据已经是归一化之后的数据了"""
-        if(min==None and max==None):
+        min和max是拟合空间的范围，用以对数据进行归一化。如果min和max都等于none，说明点数据已经是归一化之后的数据了"""
+        num=dataPoints.shpae[0]
+        self.ys = value.reshape((num, 1))
+
+        if(min is None and max is None):
             self.points=dataPoints
         else:
             self.points=self.uniformization(dataPoints,min,max)
-        points=self.points
-        num=points.shape[0]
-        d=points.shape[1]
+            self.min=min
+            self.max=max
 
-        #设置计算相关系数中所使用的theta和p值
-        self.theta=np.zeros(d)
+        points = self.points
+        num = points.shape[0]
+        d = points.shape[1]
         self.p=np.zeros(d)
-        for i in range(0,d):
-            self.theta[i]=10
-            self.p[i]=2
-        self.theta[0]=0.11
-        self.theta[1]=0.5
+        self.theta=np.zeros(d)
+        self.optimize()
+        # points = self.points
+        # num = points.shape[0]
+        # d = points.shape[1]
+        # self.theta = np.zeros(d)
+        # self.p = np.zeros(d)
+        # for i in range(0, d):
+        #     self.theta[i] = 10
+        #     self.p[i] = 2
+        # self.theta[0] = 16.9709375
+        # self.theta[1] = 16.9709375
+        #
+        # self.R = np.zeros((num, num))
+        # for i in range(0, num):
+        #     for j in range(0, num):
+        #         self.R[i, j] = self.correlation(points[i, :], points[j, :])
+        # self.R_1 = np.linalg.inv(self.R)
+        # print(np.dot(self.R_1,self.R))
+        # F = np.zeros((num, 1)) + 1
+        # R_1 = self.R_1
+        # self.ys = value.reshape((num, 1))
+        # ys = self.ys
+        #
+        # beta0 = np.dot(F.T, R_1)
+        # denominator = np.dot(beta0, F)
+        # numerator = np.dot(beta0, ys)
+        # beta0 = numerator / denominator
+        # self.beta0 = beta0
+        #
+        # factor = ys - beta0 * F
+        # sigma2 = np.dot(factor.T, R_1)
+        # sigma2 = np.dot(sigma2, factor) / num
+        # self.sigma2 = sigma2
 
-        self.R=np.zeros((num,num))
-        for i in range(0,num):
-            for j in range(0,num):
-                self.R[i,j]=self.correlation(points[i,:],points[j,:])
-        self.R_1=np.linalg.inv(self.R)
-        print(np.dot(self.R_1,self.R))
-        F=np.zeros((num,1))+1
-        R_1=self.R_1
-        self.ys = value.reshape((num, 1))
-        ys=self.ys
+    def log_likelihood(self,X):
 
-        R_1=np.eye(R_1.shape[0])
-        beta0=np.dot(F.T,R_1)
-        denominator=np.dot(beta0,F)
-        numerator=np.dot(beta0,ys)
-        beta0=numerator/denominator
-        self.beta0=beta0
+        points = self.points
+        num = points.shape[0]
+        d = points.shape[1]
+        for i in range(d):
+            self.p[i]=X[i]
+            self.theta[i]=X[i+d]
 
-        factor=ys-beta0*F
-        sigma2=np.dot(factor.T,R_1)
-        sigma2=np.dot(sigma2,factor)/num
-        self.sigma2=sigma2
+        self.R = np.zeros((num, num))
+        for i in range(0, num):
+            for j in range(0, num):
+                self.R[i, j] = self.correlation(points[i, :], points[j, :])
+        self.R_1 = np.linalg.inv(self.R)
+
+        F = np.zeros((num, 1)) + 1
+        R_1 = self.R_1
+        ys = self.ys
+
+        beta0 = np.dot(F.T, R_1)
+        denominator = np.dot(beta0, F)
+        numerator = np.dot(beta0, ys)
+        beta0 = numerator / denominator
+        self.beta0 = beta0
+
+        factor = ys - beta0 * F
+        sigma2 = np.dot(factor.T, R_1)
+        sigma2 = np.dot(sigma2, factor) / num
+        self.sigma2 = sigma2
+
+        return -num/2*np.log(sigma2**2)-0.5*np.log(np.linalg.det(self.R))
+
+
+    def optimize(self):
+        # 设置计算相关系数中所使用的theta和p值
+        min=[0,0,0,0]
+        max=[3,3,100,100]
+        test = DE(min, max, 100, 0.5, 0.5, func,False)
+        ind = test.evolution(generation=100)
+
+
 
     def getY(self,x):
         num=self.points.shape[0]
+        for i in range(x.shape[0]):
+            x[i]=(x[i]-self.min[i])/(self.max[i]-self.min[i])
         r=np.zeros((num,1))
         for i in range(0,num):
             r[i]=self.correlation(self.points[i,:],x)
@@ -64,14 +121,14 @@ class Kriging(object):
         y=np.dot(r.T,R_1)
         y=self.beta0+np.dot(y,factor)
 
-        f1=np.dot(F.T,R_1)
-        f1=(1-np.dot(f1,r))**2
-        f2=np.dot(F.T,R_1)
-        f2=np.dot(f2,F)
-        f1=f1/f2
-        f2=np.dot(r.T,R_1)
-        f2=np.dot(f2,r)
-        self.varience=self.sigma2*(1-f2+f1)
+        # f1=np.dot(F.T,R_1)
+        # f1=(1-np.dot(f1,r))**2
+        # f2=np.dot(F.T,R_1)
+        # f2=np.dot(f2,F)
+        # f1=f1/f2
+        # f2=np.dot(r.T,R_1)
+        # f2=np.dot(f2,r)
+        # self.varience=self.sigma2*(1-f2+f1)
         return y
 
     def uniformization(self,points,min,max):
@@ -103,42 +160,71 @@ class Kriging(object):
 
 
 if __name__=="__main__":
-    def func(x1,x2):
-        return 100*(x1**2-x2)**2+(1-x1)**2
-    sampleNum=20
-    min=np.array([-2.048,-2.048])
-    max=np.array([2.048,2.048])
-    lh=doe.LatinHypercube(2,sampleNum,min,max)
+    def func(X):
+        x = X[0]
+        y = X[1]
+        return 3 * (1 - x) ** 2 * np.exp(-(x ** 2) - (y + 1) ** 2) - 10 * (x / 5 - x ** 3 - y ** 5) * np.exp(
+            -x ** 2 - y ** 2) - 1 / 3 * np.exp(-(x + 1) ** 2 - y ** 2)
+    min = np.array([-3, -3])
+    max = np.array([3, 3])
+
+    # def Branin(x):
+    #     pi=3.1415926
+    #     y=x[1]-(5*x[0]**2)/(4*pi**2)+5*x[0]/pi-6
+    #     y=y**2
+    #     y+=10*(1-1/(8*pi))*np.cos(x[0])+10
+    #     return y
+    # min = np.array([-5, 0])
+    # max = np.array([10, 15])
+
+
+
+
+
+
+    x, y = np.mgrid[min[0]:max[0]:100j, min[1]:max[1]:100j]
+    s = np.zeros_like(x)
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            a = [x[i, j], y[i, j]]
+            s[i, j] = func(a)
+
+    surf = mlab.imshow(x, y, s)
+    mlab.outline()
+    mlab.axes(xlabel='x', ylabel='y', zlabel='z')
+
+    sampleNum=50
+
+    lh=DOE.LatinHypercube(2,sampleNum,min,max)
+    sample=lh.samples
+    realSample=lh.realSamples
+    # sample=np.loadtxt('samples.txt')
+    # print(sample)
+    # realSample=np.zeros_like(sample)
+    # for i in range(sample.shape[0]):
+    #     realSample[i,0]=(max[0]-min[0])*sample[i,0]+min[0]
+    #     realSample[i, 1] = (max[1] - min[1]) * sample[i, 1] + min[1]
+
     value=np.zeros(sampleNum)
     for i in range(0,sampleNum):
-        value[i]=func(lh.realSamples[i,0],lh.realSamples[i,1])
-    kriging=Kriging(lh.samples,value)
+        a = [realSample[i, 0], realSample[i, 1]]
+        value[i]=func(a)
+    kriging = Kriging(realSample, value, min, max)
 
-    numy=10
-    numx=10
-    x=np.arange(min[0],max[0],0.2)
-    y=np.arange(min[1],max[1],0.2)
-    numx=x.shape[0]
-    numy=y.shape[0]
-    X,Y=np.meshgrid(x,y)
-    value1=np.zeros((numy,numx))
-    value2=np.zeros((numy,numx))
-    for i in range(0,numx):
-        for j in range(0,numy):
-            value1[j,i]=kriging.getY(np.array([X[j,i],Y[j,i]]))
-            value2[j,i]=func(X[j,i],Y[j,i])
-    fig=plt.figure()
-    ax1=fig.add_subplot(121)
-    map1=ax1.imshow(value1,cmap=cm.get_cmap('Blues'))
-    plt.colorbar(mappable=map1, cax=None, ax=None,shrink=0.5)
-    ax1.set_title('estimate')
 
-    ax2=fig.add_subplot(122)
-    map2=ax2.imshow(value2,cmap=cm.get_cmap('Blues'))
-    plt.colorbar(mappable=map2, cax=None, ax=None,shrink=0.5)
-    ax2.set_title('real')
+    prevalue=np.zeros_like(value)
+    for i in range(prevalue.shape[0]):
+        a = [realSample[i, 0], realSample[i, 1]]
+        prevalue[i]=kriging.getY(np.array(a))
+    print(value-prevalue)
+    mlab.points3d(realSample[:,0],realSample[:,1],value-prevalue,scale_factor=0.3)
 
-    # CS = plt.contour(X, Y, value1)
-    # plt.clabel(CS, inline=0, fontsize=10)
-    # plt.title('Simplest default with labels')
-    plt.show()
+
+    preValue=np.zeros_like(x)
+    for i in range(0,x.shape[0]):
+        for j in range(0,x.shape[1]):
+            a=[x[i, j], y[i, j]]
+            preValue[i,j]=kriging.getY(np.array(a))
+    # mlab.points3d(x,y,preValue,scale_factor=0.1)
+    mlab.imshow(x, y, preValue)
+    mlab.show()
